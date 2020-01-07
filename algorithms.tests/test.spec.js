00cff38c666948e823fc4f49c12907ee2fb37391
Rostyslav Synenko <rostyslav.synenko@gmail.com>
2020-01-07 19:32:20 +0200

    ...

--- algorithms.tests/test.spec.js
+++ algorithms.tests/test.spec.js
@@ -48,15 +48,76 @@ describe('firstSumOfSquares', () => {
     assert.lengthOf(firstSumOfSquares(3), 3);
   });
 
-  it('should return an array [1, 1, 2]', () => {
-    assert.sameOrderedMembers(firstSumOfSquares(6), [1, 1, 2]);
-  });
-
   it('sum of squares is equal to 6', () => {
     assert.equal(
       firstSumOfSquares(6).reduce((acc, curr) => acc + Math.pow(curr, 2), 0),
       6
     );
+  });
+
+  describe('for input n should return an array with result', () => {
+    const inputs = [3, 6, 9, 11, 12, 14];
+    const results = [
+      [1, 1, 1],
+      [1, 1, 2],
+      [1, 2, 2],
+      [1, 1, 3],
+      [2, 2, 2],
+      [1, 2, 3]
+    ];
+
+    const makeTest = x => {
+      it(`if input is ${inputs[x]} should return an array ${JSON.stringify(
+        results[x]
+      )}`, () => {
+        assert.sameOrderedMembers(firstSumOfSquares(inputs[x]), results[x]);
+      });
+    };
+
+    for (let i = 0; i < inputs.length; i++) {
+      makeTest(i);
+    }
+  });
+
+  describe('for input n should return an empty array', () => {
+    const inputs = [1, 2, 4, 5, 7, 8, 10];
+
+    const makeTest = x => {
+      it(`if input is ${inputs[x]} should return an empty array []`, () => {
+        assert.isEmpty(firstSumOfSquares(inputs[x]));
+      });
+    };
+
+    for (let i = 0; i < inputs.length; i++) {
+      makeTest(i);
+    }
+  });
+
+  describe('sum of squares in the array should be equal to n', () => {
+    const arrays = [
+      [1, 1, 1],
+      [1, 1, 2],
+      [1, 2, 2],
+      [1, 1, 3],
+      [2, 2, 2],
+      [1, 2, 3]
+    ];
+    const results = [3, 6, 9, 11, 12, 14];
+
+    const makeTest = x => {
+      it(`sum of squares in ${JSON.stringify(arrays[x])} is equal to ${
+        results[x]
+      }`, () => {
+        assert.equal(
+          arrays[x].reduce((acc, curr) => acc + Math.pow(curr, 2), 0),
+          results[x]
+        );
+      });
+    };
+
+    for (let i = 0; i < arrays.length; i++) {
+      makeTest(i);
+    }
   });
 });
 
@@ -73,5 +134,112 @@ describe('sumOfSquaresAll', () => {
 
   it('should return an array of length 3', () => {
     assert.lengthOf(sumOfSquaresAll(6), 3);
+  });
+
+  describe('for input n should return an array with result array/arrays', () => {
+    const inputs = [3, 6, 9, 11, 12, 14];
+    const results = [
+      [[1, 1, 1]],
+      [
+        [1, 1, 2],
+        [1, 2, 1],
+        [2, 1, 1]
+      ],
+      [
+        [1, 2, 2],
+        [2, 1, 2],
+        [2, 2, 1]
+      ],
+      [
+        [1, 1, 3],
+        [1, 3, 1],
+        [3, 1, 1]
+      ],
+      [[2, 2, 2]],
+      [
+        [1, 2, 3],
+        [1, 3, 2],
+        [2, 1, 3],
+        [2, 3, 1],
+        [3, 1, 2],
+        [3, 2, 1]
+      ]
+    ];
+
+    const makeTest = x => {
+      it(`if input is ${inputs[x]} should return an array ${JSON.stringify(
+        results[x]
+      )}`, () => {
+        assert.sameDeepOrderedMembers(sumOfSquaresAll(inputs[x]), results[x]);
+      });
+    };
+
+    for (let i = 0; i < inputs.length; i++) {
+      makeTest(i);
+    }
+  });
+
+  describe('for input n should return an empty array', () => {
+    const inputs = [1, 2, 4, 5, 7, 8, 10];
+
+    const makeTest = x => {
+      it(`if input is ${inputs[x]} should return an empty array []`, () => {
+        assert.isEmpty(sumOfSquaresAll(inputs[x]));
+      });
+    };
+
+    for (let i = 0; i < inputs.length; i++) {
+      makeTest(i);
+    }
+  });
+
+  describe('sum of squares for each subarray in the array should be equal to n', () => {
+    const arrays = [
+      [[1, 1, 1]],
+      [
+        [1, 1, 2],
+        [1, 2, 1],
+        [2, 1, 1]
+      ],
+      [
+        [1, 2, 2],
+        [2, 1, 2],
+        [2, 2, 1]
+      ],
+      [
+        [1, 1, 3],
+        [1, 3, 1],
+        [3, 1, 1]
+      ],
+      [[2, 2, 2]],
+      [
+        [1, 2, 3],
+        [1, 3, 2],
+        [2, 1, 3],
+        [2, 3, 1],
+        [3, 1, 2],
+        [3, 2, 1]
+      ]
+    ];
+
+    const results = [3, 6, 9, 11, 12, 14];
+
+    const makeTest = x => {
+      it(`sum of squares for each subarray in ${JSON.stringify(
+        arrays[x]
+      )} is equal to ${results[x]}`, () => {
+        assert.isTrue(
+          arrays[x].every(
+            subarr =>
+              subarr.reduce((acc, curr) => acc + Math.pow(curr, 2), 0) ===
+              results[x]
+          )
+        );
+      });
+    };
+
+    for (let i = 0; i < arrays.length; i++) {
+      makeTest(i);
+    }
   });
 });
